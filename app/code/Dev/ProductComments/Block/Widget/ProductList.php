69229e0091fcca9b9bc17df6656b94a2e0cda626
cany <ex.a.sapovalovs@gmail.com>
2019-08-30 09:28:20 +0300

    TICKET-476 final widget version

--- app/code/Dev/ProductComments/Block/Widget/ProductList.php
+++ app/code/Dev/ProductComments/Block/Widget/ProductList.php
@@ -1,134 +1,92 @@
 <?php
+
 namespace Dev\ProductComments\Block\Widget;
 
-use Magento\Framework\App\ObjectManager;
+use Magento\Catalog\Block\Product\AbstractProduct;
+use Magento\Catalog\Block\Product\Context;
+use Magento\Catalog\Block\Product\Widget\Html\Pager;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Catalog\Pricing\Price\FinalPrice;
+use Magento\CatalogWidget\Model\Rule;
 use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Framework\Pricing\PriceCurrencyInterface;
-use Magento\Framework\Serialize\Serializer\Json;
-use Magento\Review\Model\Rss;
+use Magento\Framework\Pricing\Render;
+use Magento\Framework\View\Element\AbstractBlock;
+use Magento\Rule\Model\Condition\Sql\Builder;
 use Magento\Widget\Block\BlockInterface;
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\Event\ManagerInterface;
+use Magento\Widget\Helper\Conditions;
 
 /**
  * Catalog Product List widget block
  * Class ProductList
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct implements BlockInterface, IdentityInterface
+class ProductList extends AbstractProduct implements BlockInterface, IdentityInterface
 {
-    /**
-     * Default value for products count that will be shown
-     */
-    const DEFAULT_PRODUCTS_COUNT = 10;
-
-    /**
-     * Name of request parameter for page number value
-     *
-     * @deprecated
-     */
-    const PAGE_VAR_NAME = 'np';
-
     /**
      * Default value for products per page
      */
-    const DEFAULT_PRODUCTS_PER_PAGE = 5;
-
+    const DEFAULT_PRODUCTS_PER_PAGE = 4;
     /**
      * Default value whether show pager or not
      */
     const DEFAULT_SHOW_PAGER = false;
-
     /**
      * Instance of pager block
      *
-     * @var \Magento\Catalog\Block\Product\Widget\Html\Pager
+     * @var Pager
      */
     protected $pager;
-
     /**
      * @var \Magento\Framework\App\Http\Context
      */
     protected $httpContext;
-
     /**
      * Catalog product visibility
      *
-     * @var \Magento\Catalog\Model\Product\Visibility
+     * @var Visibility
      */
     protected $catalogProductVisibility;
-
     /**
      * Product collection factory
      *
-     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     * @var CollectionFactory
      */
     protected $productCollectionFactory;
-
     /**
-     * @var \Magento\Rule\Model\Condition\Sql\Builder
+     * @var Builder
      */
     protected $sqlBuilder;
-
     /**
-     * @var \Magento\CatalogWidget\Model\Rule
+     * @var Rule
      */
     protected $rule;
-
     /**
-     * @var \Magento\Widget\Helper\Conditions
+     * @var Conditions
      */
     protected $conditionsHelper;
 
     /**
-     * @var PriceCurrencyInterface
-     */
-    private $priceCurrency;
-
-    /**
-     * Json Serializer Instance
-     *
-     * @var Json
-     */
-    private $json;
-    /**
-     * @var ProductRepositoryInterface
-     */
-    private $productRepository;
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $criteriaBuilder;
-
-    /**
-     * Application Event Dispatcher
-     *
-     * @var \Magento\Framework\Event\ManagerInterface
-     */
-    protected $eventManager;
-
-    /**
-     * @param \Magento\Catalog\Block\Product\Context $context
-     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
-     * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
+     * @param Context $context
+     * @param CollectionFactory $productCollectionFactory
+     * @param Visibility $catalogProductVisibility
      * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param \Magento\Rule\Model\Condition\Sql\Builder $sqlBuilder
-     * @param \Magento\CatalogWidget\Model\Rule $rule
-     * @param \Magento\Widget\Helper\Conditions $conditionsHelper
+     * @param Builder $sqlBuilder
+     * @param Rule $rule
+     * @param Conditions $conditionsHelper
      * @param array $data
-     * @param Json|null $json
      */
     public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
-        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
+        Context $context,
+        CollectionFactory $productCollectionFactory,
+        Visibility $catalogProductVisibility,
         \Magento\Framework\App\Http\Context $httpContext,
-        \Magento\Rule\Model\Condition\Sql\Builder $sqlBuilder,
-        \Magento\CatalogWidget\Model\Rule $rule,
-        \Magento\Widget\Helper\Conditions $conditionsHelper,
-        array $data = [],
-        Json $json = null
+        Builder $sqlBuilder,
+        Rule $rule,
+        Conditions $conditionsHelper,
+        array $data = []
     ) {
         $this->productCollectionFactory = $productCollectionFactory;
         $this->catalogProductVisibility = $catalogProductVisibility;
@@ -136,7 +94,6 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
         $this->sqlBuilder = $sqlBuilder;
         $this->rule = $rule;
         $this->conditionsHelper = $conditionsHelper;
-        $this->json = $json ?: ObjectManager::getInstance()->get(Json::class);
         parent::__construct(
             $context,
             $data
@@ -145,54 +102,12 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
 
     /**
      * {@inheritdoc}
-     */
-    protected function _construct()
-    {
-        parent::_construct();
-        $this->addColumnCountLayoutDepend('empty', 6)
-            ->addColumnCountLayoutDepend('1column', 5)
-            ->addColumnCountLayoutDepend('2columns-left', 4)
-            ->addColumnCountLayoutDepend('2columns-right', 4)
-            ->addColumnCountLayoutDepend('3columns', 3);
-
-        $this->addData([
-            'cache_lifetime' => 86400,
-            'cache_tags' => [\Magento\Catalog\Model\Product::CACHE_TAG,
-            ], ]);
-    }
-
-    /**
-     * Get key pieces for caching block content
-     *
-     * @return array
-     */
-    public function getCacheKeyInfo()
-    {
-        $conditions = $this->getData('conditions')
-            ? $this->getData('conditions')
-            : $this->getData('conditions_encoded');
-
-        return [
-            'CATALOG_PRODUCTS_LIST_WIDGET',
-            $this->getPriceCurrency()->getCurrencySymbol(),
-            $this->_storeManager->getStore()->getId(),
-            $this->_design->getDesignTheme()->getId(),
-            $this->httpContext->getValue(\Magento\Customer\Model\Context::CONTEXT_GROUP),
-            intval($this->getRequest()->getParam($this->getData('page_var_name'), 1)),
-            $this->getProductsPerPage(),
-            $conditions,
-            $this->json->serialize($this->getRequest()->getParams())
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
      * @SuppressWarnings(PHPMD.NPathComplexity)
      */
     public function getProductPriceHtml(
-        \Magento\Catalog\Model\Product $product,
+        Product $product,
         $priceType = null,
-        $renderZone = \Magento\Framework\Pricing\Render::ZONE_ITEM_LIST,
+        $renderZone = Render::ZONE_ITEM_LIST,
         array $arguments = []
     ) {
         if (!isset($arguments['zone'])) {
@@ -207,14 +122,12 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
         $arguments['display_minimal_price'] = isset($arguments['display_minimal_price'])
             ? $arguments['display_minimal_price']
             : true;
-
-        /** @var \Magento\Framework\Pricing\Render $priceRender */
+        /** @var Render $priceRender */
         $priceRender = $this->getLayout()->getBlock('product.price.render.default');
-
         $price = '';
         if ($priceRender) {
             $price = $priceRender->render(
-                \Magento\Catalog\Pricing\Price\FinalPrice::PRICE_CODE,
+                FinalPrice::PRICE_CODE,
                 $product,
                 $arguments
             );
@@ -223,137 +136,19 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
     }
 
     /**
-     * {@inheritdoc}
-     */
-    protected function _beforeToHtml()
-    {
-        $this->setProductCollection($this->createCollection());
-        return parent::_beforeToHtml();
-    }
-
-    /**
-     * Prepare and return product collection
-     *
-     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
-     */
-    public function createCollection()
-    {
-        /** @var $collection \Magento\Catalog\Model\ResourceModel\Product\Collection */
-        $collection = $this->productCollectionFactory->create();
-        $collection->setVisibility($this->catalogProductVisibility->getVisibleInCatalogIds());
-
-        $collection = $this->_addProductAttributesAndPrices($collection)
-            ->addStoreFilter()
-            ->setPageSize($this->getPageSize())
-            ->addAttributeToFilter('product_comments', 1)
-            ->setCurPage($this->getRequest()->getParam($this->getData('page_var_name'), 1));
-
-        $conditions = $this->getConditions();
-        $conditions->collectValidatedAttributes($collection);
-        $this->sqlBuilder->attachConditionToCollection($collection, $conditions);
-
-        /**
-         * Prevent retrieval of duplicate records. This may occur when multiselect product attribute matches
-         * several allowed values from condition simultaneously
-         */
-        $collection->distinct(true);
-
-        return $collection;
-    }
-
-    /**
-     * @return \Magento\Rule\Model\Condition\Combine
-     */
-    protected function getConditions()
-    {
-        $conditions = $this->getData('conditions_encoded')
-            ? $this->getData('conditions_encoded')
-            : $this->getData('conditions');
-
-        if ($conditions) {
-            $conditions = $this->conditionsHelper->decode($conditions);
-        }
-
-        foreach ($conditions as $key => $condition) {
-            if (!empty($condition['attribute'])
-                && in_array($condition['attribute'], ['special_from_date', 'special_to_date'])
-            ) {
-                $conditions[$key]['value'] = date('Y-m-d H:i:s', strtotime($condition['value']));
-            }
-        }
-
-        $this->rule->loadPost(['conditions' => $conditions]);
-        return $this->rule->getConditions();
-    }
-
-    /**
-     * Retrieve how many products should be displayed
-     *
-     * @return int
-     */
-    public function getProductsCount()
-    {
-        if ($this->hasData('products_count')) {
-            return $this->getData('products_count');
-        }
-
-        if (null === $this->getData('products_count')) {
-            $this->setData('products_count', self::DEFAULT_PRODUCTS_COUNT);
-        }
-
-        return $this->getData('products_count');
-    }
-
-    /**
-     * Retrieve how many products should be displayed
-     *
-     * @return int
-     */
-    public function getProductsPerPage()
-    {
-        if (!$this->hasData('products_per_page')) {
-            $this->setData('products_per_page', self::DEFAULT_PRODUCTS_PER_PAGE);
-        }
-        return $this->getData('products_per_page');
-    }
-
-    /**
-     * Return flag whether pager need to be shown or not
-     *
-     * @return bool
-     */
-    public function showPager()
-    {
-        if (!$this->hasData('show_pager')) {
-            $this->setData('show_pager', self::DEFAULT_SHOW_PAGER);
-        }
-        return (bool)$this->getData('show_pager');
-    }
-
-    /**
-     * Retrieve how many products should be displayed on page
-     *
-     * @return int
-     */
-    protected function getPageSize()
-    {
-        return $this->showPager() ? $this->getProductsPerPage() : $this->getProductsCount();
-    }
-
-    /**
      * Render pagination HTML
      *
      * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function getPagerHtml()
     {
         if ($this->showPager() && $this->getProductCollection()->getSize() > $this->getProductsPerPage()) {
             if (!$this->pager) {
                 $this->pager = $this->getLayout()->createBlock(
-                    \Magento\Catalog\Block\Product\Widget\Html\Pager::class,
-                    'widget.products.list.pager'
+                    Pager::class,
+                    'widget.product.comments.pager'
                 );
-
                 $this->pager->setUseContainer(true)
                     ->setShowAmounts(true)
                     ->setShowPerPage(false)
@@ -362,7 +157,7 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
                     ->setTotalLimit($this->getProductsCount())
                     ->setCollection($this->getProductCollection());
             }
-            if ($this->pager instanceof \Magento\Framework\View\Element\AbstractBlock) {
+            if ($this->pager instanceof AbstractBlock) {
                 return $this->pager->toHtml();
             }
         }
@@ -384,8 +179,7 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
                 }
             }
         }
-
-        return $identities ?: [\Magento\Catalog\Model\Product::CACHE_TAG];
+        return $identities ?: [Product::CACHE_TAG];
     }
 
     /**
@@ -399,17 +193,81 @@ class ProductList extends \Magento\Catalog\Block\Product\AbstractProduct impleme
     }
 
     /**
-     * @return PriceCurrencyInterface
-     *
-     * @deprecated 100.2.0
+     * {@inheritdoc}
      */
-    private function getPriceCurrency()
+    protected function _beforeToHtml()
     {
-        if ($this->priceCurrency === null) {
-            $this->priceCurrency = \Magento\Framework\App\ObjectManager::getInstance()
-                ->get(PriceCurrencyInterface::class);
-        }
-        return $this->priceCurrency;
+        $this->setProductCollection($this->createCollection());
+        return parent::_beforeToHtml();
     }
-    
+
+    /**
+     * Prepare and return product collection
+     *
+     * @return Collection
+     */
+    public function createCollection()
+    {
+        /** @var $collection Collection */
+        $collection = $this->productCollectionFactory->create();
+        $collection->setVisibility($this->catalogProductVisibility->getVisibleInCatalogIds());
+        $collection = $this->_addProductAttributesAndPrices($collection)
+            ->addStoreFilter()
+            ->setPageSize($this->getPageSize())
+            ->addAttributeToFilter('product_comments', 1)
+            ->setCurPage($this->getRequest()->getParam($this->getData('page_var_name'), 1));
+
+        /**
+         * Prevent retrieval of duplicate records. This may occur when multiselect product attribute matches
+         * several allowed values from condition simultaneously
+         */
+        $collection->distinct(true);
+        return $collection;
+    }
+
+    /**
+     * Retrieve how many products should be displayed on page
+     *
+     * @return int
+     */
+    protected function getPageSize()
+    {
+        return $this->showPager() ? $this->getProductsPerPage() : $this->getProductsCount();
+    }
+
+    /**
+     * Return flag whether pager need to be shown or not
+     *
+     * @return bool
+     */
+    public function showPager()
+    {
+        if (!$this->hasData('show_pager')) {
+            $this->setData('show_pager', self::DEFAULT_SHOW_PAGER);
+        }
+        return (bool)$this->getData('show_pager');
+    }
+
+    /**
+     * Retrieve how many products should be displayed
+     *
+     * @return int
+     */
+    public function getProductsPerPage()
+    {
+        if (!$this->hasData('products_per_page')) {
+            $this->setData('products_per_page', self::DEFAULT_PRODUCTS_PER_PAGE);
+        }
+        return $this->getData('products_per_page');
+    }
+
+    /**
+     * Retrieve how many products should be displayed
+     *
+     * @return int
+     */
+    public function getProductsCount()
+    {
+        return $this->getData('products_count');
+    }
 }
